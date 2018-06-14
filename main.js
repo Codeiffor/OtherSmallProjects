93387a5ec717d28ba4436b88b8f4ac71f5ed3178
Codeiffor <sid96@protonmail.com>
2018-06-15 05:08:27 +0530

    chackpoint-3: graph added

--- main.js
+++ main.js
@@ -6,12 +6,35 @@ var root1;
 var root2;
 var res=document.querySelector("#result");
 var box1=res.innerHTML;
+var str;
+
+var my_graph=document.getElementById("my_graph");
+function graphRefresh(){
+  function g(m){
+      my_graph.contentWindow.postMessage(m,"http://graph.tk");
+  };
+  str="add:"+a+"*x^2+"+b+"*x+"+c;
+  g("empty");
+  g(str);
+  g("center:0,0");
+}
+
 function findRoots(){
   a=parseInt(document.querySelector("#input-a").value);
   b=parseInt(document.querySelector("#input-b").value);
   c=parseInt(document.querySelector("#input-c").value);
   dis=b*b-4*a*c;
-  if(dis>=0){
+  if(a==0){
+    root1=-c/b;
+    root2=root1;
+    root1=(Math.round(root1*100))/100;
+    root2=(Math.round(root2*100))/100;
+    dis="<p><br>The root of this Linear Equation is <strong> "+root1+"</strong>.</p>"
+    if(b==0){
+    dis="<p><br>This Equation has <strong>no real</strong> root.</p>";
+    }
+  }
+  else if(dis>=0){
     dis=Math.sqrt(dis);
     root1=(dis-b)/(4*a);
     root2=-(dis+b)/(4*a);
@@ -26,4 +49,10 @@ function findRoots(){
   document.querySelector("#input-a").value=a;
   document.querySelector("#input-b").value=b;
   document.querySelector("#input-c").value=c;
+
+  graphRefresh();
 }
+
+
+
+
